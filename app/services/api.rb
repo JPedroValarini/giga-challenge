be5f4c2cf4fff92ecc6ff146c6dd6d7f1c34bbcf
joao pedro <joaopedrovalarini@gmail.com>
2021-12-15 14:48:45 -0300

    config postgres and get api data

---
+++ app/services/api.rb
@@ -0,0 +1,15 @@
+class Api 
+  include HTTParty
+
+  BASE_URL = 'https://randomuser.me/api?format=json&results=30&inc=gender,name,email,picture&nat=br&seed=giga'.freeze
+
+  def initialize
+    @options = {}
+  end
+
+  def users_api
+    HTTParty.get("#{BASE_URL}")
+  end
+end
+
+
